2da037b6be6eb2faaddd3a274a99fd84ed6a1514
ebcrowder <eric@ebcrowder.dev>
2021-03-25 21:54:08 -0700

    Write to aws creds file

--- main.go
+++ main.go
@@ -4,15 +4,14 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
-	"os"
-	"strings"
 )
-
-var Region string       // TODO read from ~/.aws/config
-var SerialNumber string // TODO read from ~/.aws/credentials
 
 type STSGetSessionTokenAPI interface {
 	GetSessionToken(ctx context.Context,
@@ -25,12 +24,14 @@ func GetSessionToken(c context.Context, api STSGetSessionTokenAPI, input *sts.Ge
 }
 
 func main() {
-	SerialNumber := os.Getenv("SERIAL_NUMBER")
-	Region := os.Getenv("REGION")
+	CredentialsFile := os.Getenv("CREDENTIALS_FILE") // TODO set as default
+	SerialNumber := os.Getenv("SERIAL_NUMBER")       // TODO read from aws config or credentials file
+	Region := os.Getenv("REGION")                    // TODO read from aws config file
+	var DurationSeconds int32 = 900                  // TODO handle default somehow
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(Region))
 	if err != nil {
-		panic("configuration error, " + err.Error())
+		panic("Configuration error, " + err.Error())
 	}
 
 	client := sts.NewFromConfig(cfg)
@@ -41,14 +42,41 @@ func main() {
 	TokenCode, _ := reader.ReadString('\n')
 	TokenCode = strings.TrimSuffix(TokenCode, "\n")
 
-	sessionToken, err := GetSessionToken(context.TODO(), client, &sts.GetSessionTokenInput{
-		DurationSeconds: aws.Int32(900),
+	response, err := GetSessionToken(context.TODO(), client, &sts.GetSessionTokenInput{
+		DurationSeconds: aws.Int32(DurationSeconds),
 		SerialNumber:    aws.String(SerialNumber),
 		TokenCode:       aws.String(TokenCode),
 	})
 	if err != nil {
-		panic("configuration error, " + err.Error())
+		panic("Could not get session token:" + err.Error())
 	}
 
-	fmt.Println(sessionToken)
+	file, err := os.Open(CredentialsFile)
+	if err != nil {
+		panic("Could not open aws credentials file:" + err.Error())
+	}
+	defer file.Close()
+
+	input, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic("Could not open aws credentials file:" + err.Error())
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		if strings.Contains(line, "[development]") {
+			lines[i] = "[development]\n" + "aws_access_key_id" + " = " + *response.Credentials.AccessKeyId + "\n" + "aws_secret_access_key" + " = " + *response.Credentials.SecretAccessKey + "\n" + "aws_security_token" + " = " + *response.Credentials.SessionToken + "\n"
+		} else {
+			if i == len(lines)-1 {
+				lines[i] = "[development]\n" + "aws_access_key_id" + " = " + *response.Credentials.AccessKeyId + "\n" + "aws_secret_access_key" + " = " + *response.Credentials.SecretAccessKey + "\n" + "aws_security_token" + " = " + *response.Credentials.SessionToken + "\n"
+			}
+		}
+	}
+
+	output := strings.Join(lines, "\n")
+	err = ioutil.WriteFile(CredentialsFile, []byte(output), 0644)
+	if err != nil {
+		panic("Could not write to aws credentials file:" + err.Error())
+	}
 }
