220cc04c1c2e91efbc57735523448ddda99a3181
ebcrowder <eric@ebcrowder.dev>
2021-03-26 17:58:53 -0700

    Mv token code logic into fn

--- main.go
+++ main.go
@@ -24,16 +24,21 @@ func GetSessionToken(c context.Context, api STSGetSessionTokenAPI, input *sts.Ge
 	return api.GetSessionToken(c, input)
 }
 
-func generateCredentialsText(CredentialsFile string, temporaryCredentials string) []string {
-	file, err := os.Open(CredentialsFile)
+func openAndReadFile(path string) []byte {
+	file, err := os.Open(path)
 	if err != nil {
-		panic("Could not open aws credentials file:" + err.Error())
+		panic("Could not open file:" + err.Error())
 	}
 	defer file.Close()
 	input, err := ioutil.ReadAll(file)
 	if err != nil {
-		panic("Could not open aws credentials file:" + err.Error())
+		panic("Could not read file:" + err.Error())
 	}
+	return input
+}
+
+func generateCredentialsText(path, temporaryCredentials string) []string {
+	input := openAndReadFile(path)
 	lines := strings.Split(string(input), "\n")
 
 	for i, line := range lines {
@@ -49,15 +54,45 @@ func generateTemporaryCredentials(response *sts.GetSessionTokenOutput) string {
 		"aws_access_key_id" + " = " + *response.Credentials.AccessKeyId + "\n" +
 		"aws_secret_access_key" + " = " + *response.Credentials.SecretAccessKey + "\n" +
 		"aws_security_token" + " = " + *response.Credentials.SessionToken + "\n" +
-		"aws_token_expiration" + " = " + response.Credentials.Expiration.Format(time.RFC3339) + "\n"
+		"aws_token_expiration" + " = " + response.Credentials.Expiration.Format(time.RFC3339)
 	return temporaryCredentials
 }
 
+func findConfigValue(path, value string) string {
+	var match string
+	input := openAndReadFile(path)
+	lines := strings.Split(string(input), "\n")
+
+	for _, line := range lines {
+		if strings.Contains(line, value) {
+			s := strings.SplitAfter(line, value+" = ")
+			match = s[1]
+		}
+	}
+
+	if len(match) == 0 {
+		panic("Could not locate value in aws config file:" + value)
+	}
+
+	return match
+}
+
+func getTokenCodeFromStdIn() string {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Please enter your 2FA Code: ")
+
+	TokenCode, _ := reader.ReadString('\n')
+	TokenCode = strings.TrimSuffix(TokenCode, "\n")
+	return TokenCode
+}
+
 func main() {
-	CredentialsFile := os.Getenv("CREDENTIALS_FILE") // TODO set as default
-	SerialNumber := os.Getenv("SERIAL_NUMBER")       // TODO read from aws config or credentials file
-	Region := os.Getenv("REGION")                    // TODO read from aws config file
-	var DurationSeconds int32 = 900                  // TODO handle default somehow
+	CredentialsFile := os.Getenv("CREDENTIALS_FILE") // TODO set as default - command line args?
+	ConfigFile := os.Getenv("CONFIG_FILE")           // TODO set as default - command line args?
+	var DurationSeconds int32 = 900                  // TODO handle default somehow - command line args?
+	SerialNumber := findConfigValue(ConfigFile, "mfa_serial")
+	Region := findConfigValue(ConfigFile, "region")
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(Region))
 	if err != nil {
@@ -66,11 +101,7 @@ func main() {
 
 	client := sts.NewFromConfig(cfg)
 
-	// get TokenCode from stdin
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Please enter your 2FA Code: ")
-	TokenCode, _ := reader.ReadString('\n')
-	TokenCode = strings.TrimSuffix(TokenCode, "\n")
+	TokenCode := getTokenCodeFromStdIn()
 
 	response, err := GetSessionToken(context.TODO(), client, &sts.GetSessionTokenInput{
 		DurationSeconds: aws.Int32(DurationSeconds),
